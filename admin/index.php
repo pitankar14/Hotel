3a9572dc181da20bc0d051f9efe82c859f3abf5f
akshetti14 <14akshetti@gmail.com>
2020-04-27 15:07:04 +0530

    login_page

--- admin/index.php
+++ admin/index.php
@@ -1,85 +1,188 @@
-<?php
- session_start();
- if(isset($_SESSION["user"]))
- {
-      header("location:home.php");
- }
-
+<?php  
+ session_start();  
+ if(isset($_SESSION["user"]))  
+ {  
+      header("location:home.php");  
+ }  
+ 
  ?>
 <!DOCTYPE html>
 <html >
 <head>
   <meta charset="UTF-8">
   <title>MU FASA ADMIN</title>
+  
+  
+     
+      <link rel="stylesheet" href="./css/style.css">
 
-
-
-      <link rel="stylesheet" href="css/style.css">
-
-
+  
 </head>
 
 <body>
-  <div id="clouds">
+  <!-- <div id="clouds">
 	<div class="cloud x1"></div>
 	<!-- Time for multiple clouds to dance around -->
-	<div class="cloud x2"></div>
-	<div class="cloud x3"></div>
-	<div class="cloud x4"></div>
-	<div class="cloud x5"></div>
-</div>
+
 
  <div class="container">
 
-
-      <div id="login">
-
+ <div class="login-triangle"></div>
+ <h2 class="login-header">MU FASA ADMIN </h2>
         <form method="post">
 
           <fieldset class="clearfix">
 
-            <p><span class="fontawesome-user"></span><input type="text"  name="user" value="Username" onBlur="if(this.value == '') this.value = 'Username'" onFocus="if(this.value == 'Username') this.value = ''" required></p> <!-- JS because of IE support; better: placeholder="Username" -->
-            <p><span class="fontawesome-lock"></span><input type="password" name="pass"  value="Password" onBlur="if(this.value == '') this.value = 'Password'" onFocus="if(this.value == 'Password') this.value = ''" required></p> <!-- JS because of IE support; better: placeholder="Password" -->
-            <p><input type="submit" name="sub"  value="Login"></p>
 
+          <div class="login">
+
+  
+ 
+
+  <form class="login-container">
+    <p><input type="text"  name="user" value="Username" onBlur="if(this.value == '') this.value = 'Username'" onFocus="if(this.value == 'Username') this.value = ''" required>
+    <p><input type="password" name="pass"  value="Password" onBlur="if(this.value == '') this.value = 'Password'" onFocus="if(this.value == 'Password') this.value = ''" required>
+    <p><input type="submit" name="sub"  value="Login"></p>
+  </form>
+</div>
           </fieldset>
 
         </form>
 
-
+       
 
       </div> <!-- end login -->
 
     </div>
-    <div class="bottom">  <h3><a href="../index.php">MU FASA HOMEPAGE</a></h3></div>
+    <style>
+    @import url(https://fonts.googleapis.com/css?family=Open+Sans:400,700);
 
+    body {
+  background: #2c3338;
+  color: #606468;
+  font: 87.5%/1.5em 'Open Sans', sans-serif;
+  margin: 0;
+  overflow-y:auto !important;
+  overflow-x:hidden !important;
+  background-image: url('./assets/img/login_back.jpg');
+  background-size: cover;
+}
 
+.login {
+  width: 400px;
+  margin: 16px auto;
+  font-size: 16px;
+}
+
+/* Reset top and bottom margins from certain elements */
+.login-header,
+.login p {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+/* The triangle form is achieved by a CSS hack */
+.login-triangle {
+  width: 0;
+  margin-right: auto;
+  margin-left: auto;
+  border: 12px solid transparent;
+  border-bottom-color: #28d;
+}
+
+.login-header {
+  background: #28d;
+  padding: 20px;
+  font-size: 1.4em;
+  font-weight: normal;
+  text-align: center;
+  text-transform: uppercase;
+  color: #fff;
+}
+form{
+  background:#fff;
+  opacity:0.8;
+}
+.login-container {
+  background: #fff;
+  padding: 12px;
+}
+
+/* Every row inside .login-container is defined with p tags */
+.login p {
+  padding: 12px;
+}
+
+.login input {
+  box-sizing: border-box;
+  display: block;
+  width: 100%;
+  border-width: 1px;
+  border-style: solid;
+  padding: 16px;
+  outline: 0;
+  font-family: inherit;
+  font-size: 0.95em;
+}
+
+.login input[type="email"],
+.login input[type="password"] {
+  background: #fff;
+  border-color: #bbb;
+  color: #555;
+}
+
+/* Text fields' focus effect */
+.login input[type="email"]:focus,
+.login input[type="password"]:focus {
+  border-color: #888;
+}
+
+.login input[type="submit"] {
+  background: #28d;
+  border-color: transparent;
+  color: #fff;
+  cursor: pointer;
+}
+
+.login input[type="submit"]:hover {
+  background: #17c;
+}
+
+/* Buttons' focus effect */
+.login input[type="submit"]:focus {
+  border-color: #05a;
+}
+    </style>
+    <!-- <div class="bottom">  <h3><a href="../index.php">SUN RISE HOMEPAGE</a></h3></div> -->
+  
+  
 </body>
 </html>
 
 <?php
    include('db.php');
-
-
+  
+   
    if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form
-
+      // username and password sent from form 
+      
       $myusername = mysqli_real_escape_string($con,$_POST['user']);
-      $mypassword = mysqli_real_escape_string($con,$_POST['pass']);
-
+      $mypassword = mysqli_real_escape_string($con,$_POST['pass']); 
+      
       $sql = "SELECT id FROM login WHERE usname = '$myusername' and pass = '$mypassword'";
       $result = mysqli_query($con,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
-
+      
       $count = mysqli_num_rows($result);
-
+      
       // If result matched $myusername and $mypassword, table row must be 1 row
-
+		
       if($count == 1) {
-
+         
          $_SESSION['user'] = $myusername;
-
+         
          header("location: home.php");
       }else {
          echo '<script>alert("Your Login Name or Password is invalid") </script>' ;
